b8abafd6bc327640e05fe7206d45170dff167bbd
Robb Glasser <rglasser@google.com>
2022-01-18 16:03:24 +0300

    X2: rootdir: Enable idle_state mechanism Allows us to check the idle state of the display in order to use perf hints to improve system performance.
    Bug: 69270928
    Bug: 69915886
    
    Test: Booted and cat-ed the affected files
    Change-Id: I8717093e9b8ed9ed35b6ca51a20225abbf71f775

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3355,6 +3355,10 @@ case "$target" in
     		echo 2-5     > /dev/cpuset/system-background/cpus
     		echo 2-5     > /dev/cpuset/restricted/cpus
 
+    		# Enable idle state listener
+   			echo 1 > /sys/class/drm/card0/device/idle_encoder_mask
+    		echo 100 > /sys/class/drm/card0/device/idle_timeout_ms
+
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
             ;;
@@ -3452,6 +3456,10 @@ case "$target" in
    				echo 4-5     > /dev/cpuset/background/cpus
     			echo 2-5     > /dev/cpuset/system-background/cpus
     			echo 2-5     > /dev/cpuset/restricted/cpus
+
+    			# Enable idle state listener
+    			echo 1 > /sys/class/drm/card0/device/idle_encoder_mask
+    			echo 100 > /sys/class/drm/card0/device/idle_timeout_ms
 
                 # Turn on sleep modes.
                 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
