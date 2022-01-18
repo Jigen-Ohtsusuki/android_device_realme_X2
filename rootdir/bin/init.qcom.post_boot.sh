2478abba6970ecc26c9b71eecac0d91ffef946ec
Kyle Lin <kylelin@google.com>
2022-01-18 15:12:09 +0300

    X2: rootdir: Modify the cpuset setting Because the arch is similiar with SDM670, we aling the setting with bonito.
    Bug: 142552311
    Test: build , munaual check the settings
    Change-Id: Ica44125c8ed71a85cfd6bde77c9107bceed7b094

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3349,8 +3349,11 @@ case "$target" in
       setprop vendor.dcvs.prop 1
 
             # cpuset parameters
-            echo 0-5 > /dev/cpuset/background/cpus
-            echo 0-5 > /dev/cpuset/system-background/cpus
+            echo 0-7     > /dev/cpuset/top-app/cpus
+    		echo 0-5,7   > /dev/cpuset/foreground/cpus
+    		echo 4-5     > /dev/cpuset/background/cpus
+    		echo 2-5     > /dev/cpuset/system-background/cpus
+    		echo 2-5     > /dev/cpuset/restricted/cpus
 
             # Turn on sleep modes.
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
@@ -3444,8 +3447,11 @@ case "$target" in
             setprop vendor.dcvs.prop 1
 
             # cpuset parameters
-                echo 0-5 > /dev/cpuset/background/cpus
-                echo 0-5 > /dev/cpuset/system-background/cpus
+            	echo 0-7     > /dev/cpuset/top-app/cpus
+                echo 0-5,7   > /dev/cpuset/foreground/cpus
+   				echo 4-5     > /dev/cpuset/background/cpus
+    			echo 2-5     > /dev/cpuset/system-background/cpus
+    			echo 2-5     > /dev/cpuset/restricted/cpus
 
                 # Turn on sleep modes.
                 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
