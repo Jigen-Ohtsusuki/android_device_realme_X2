f852553aa422f133549019b16a447b4c4ea00769
johnmart19 <johnivan19999@gmail.com>
2023-05-30 07:31:23 +0800

    X2: bluetooth: Compile missing lib for ldac
    I a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:135 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    E a2dp_vendor_ldac: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac.cc:1418 init: init: cannot load the decoder

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth LDAC
+PRODUCT_PACKAGES += \
+    libldacBT_bco
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2240
 TARGET_SCREEN_WIDTH := 1080
