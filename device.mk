7ede679f7604c5a35256decf18f281ffacc39679
Mayur <ultramayur123@gmail.com>
2023-08-28 07:40:55 +0000

    X2: textclassifier: add and ship prebuilt textclassifier
    * Fixes the late display of copied texts on the clipboard. Also fixes text copy issues in browsers.
    * Thanks to Mayur Varde(marshmello61) for this fix.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: AceBlade007x <mayurbelorkar8@gmail.com>

--- device.mk
+++ device.mk
@@ -359,6 +359,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.min.fling_velocity=160 \
     ro.max.fling_velocity=20000
 
+# TextClassifier
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/textclassifier/actions_suggestions.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/actions_suggestions.universal.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.en.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.en.model \
+    $(LOCAL_PATH)/textclassifier/lang_id.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/lang_id.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.universal.model
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
