ce0738e6fe0789ad6cc0d88d35d55e83bc083211
Nikhil Bhandare <40919273+nik8424@users.noreply.github.com>
2023-09-10 14:05:47 +0530

    configs: Switch To AOSP surfaceflinger

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,9 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # UDFPS
 TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.x2
 TARGET_USES_FOD_ZPOS := true
