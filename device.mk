0f309013c1360df8b5845cb7eb699999bb5f81cc
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2023-10-01 13:04:57 +0530

    Optimize Launcher3 and Settings for speed

--- device.mk
+++ device.mk
@@ -120,6 +120,19 @@ PRODUCT_PACKAGES += \
 
 # Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    NexusLauncherRelease \
+    Settings \
+    SystemUIGoogle
+
+# Dex
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
     SystemUI
 
 # Display
