e9053212cbf67a1cc9c209baec60c6fbd3c35ee5
Pranav Vashi <neobuddy89@gmail.com>
2024-02-08 15:06:58 +0000

    X2:  Add dolby to cloned apps exemption list Change-Id: I47ab06f7d5152c4c993359ac85d121e41c8df235
    Signed-off-by: AceBlade007x <acebladelevi007x@gmail.com>

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Clone apps exemption
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/preinstalled-packages-platform-realme-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-realme-product.xml
+
 # Dex/ART optimization
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
