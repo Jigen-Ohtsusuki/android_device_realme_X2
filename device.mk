45079563a49a3453e94e7ba810a0fa06819d171d
ZVNexus <akoskovich@pm.me>
2024-04-06 03:41:00 +0000

    X2: Declare support for device ID attestation Change-Id: I0365d6726d687af290b1ba439fd558c4445f5d7f
    Signed-off-by: AceBlade007x <acebladelevi007x@gmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Attestation
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
