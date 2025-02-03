81f6306ae7d67429749675dd483159a02160f437
Jigen-Ohtsusuki <abhishekpenkey@gmail.com>
2025-02-03 18:26:55 +0000

    fix duplication

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,6 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # SurfaceFlinger
